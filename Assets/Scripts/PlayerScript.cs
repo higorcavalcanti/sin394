5bd7c8986928083317f725b392e14b88688d4d03
Higor <higordaniell@live.com>
2016-05-08 00:07:07 -0300

    Personagem andando no level 1

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float playerSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		float amtToMoveX = Input.GetAxisRaw ("Horizontal") * playerSpeed * Time.deltaTime;
+		float amtToMoveY = Input.GetAxisRaw ("Vertical") * playerSpeed * Time.deltaTime;
+		
+		transform.Translate ( Vector3.right * amtToMoveX, Space.World );
+		transform.Translate ( Vector3.up * amtToMoveY, Space.World );	
+	}
+}
